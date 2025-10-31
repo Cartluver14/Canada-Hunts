07be9712359de4bb36c8cdcbc2da6d5a206c3706
339713067 <339713067@2260I-L29-023>
2025-10-31 09:39:09 -0400

    not done

--- Game1.cs
+++ Game1.cs
@@ -11,15 +11,24 @@ namespace Canada_Hunts
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Vector2 goosespeed= new Vector2 (-2,0);
+
+        Vector2 moosespeed= new Vector2 (2,0);
 
 
         Random generator;
         List<Rectangle> gooserectangle;
 
+        List<Rectangle> mooserectangle;
+
+        List<Texture2D> moosetextures;
+
+
+
         List<Texture2D> goosetextures;
         Random random = new Random();
         Rectangle window;
-        Texture2D canadabg,gooseTexture;
+        Texture2D canadabg,gooseTexture,mooseTexture;
 
 
         float seconds;
@@ -42,6 +51,8 @@ namespace Canada_Hunts
            
             gooserectangle = new List<Rectangle>();
             goosetextures = new List<Texture2D>();
+            mooserectangle = new List<Rectangle>();
+            moosetextures = new List<Texture2D>();
             for (int i = 0; i < 5; i++)
             {
                 gooserectangle.Add(new Rectangle(
@@ -49,6 +60,19 @@ namespace Canada_Hunts
                     generator.Next(0, 130 ),
                     50,
                     50
+                    ));
+
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                mooserectangle.Add(new Rectangle(
+                    generator.Next(0, window.Width - 50),
+                    325,
+
+                   
+                    80,
+                    80
                     ));
 
 
@@ -67,6 +91,7 @@ namespace Canada_Hunts
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             canadabg = Content.Load<Texture2D>("Images/canadabg");
             gooseTexture = Content.Load<Texture2D>("Images/goose");
+            mooseTexture = Content.Load<Texture2D>("Images/moose");
 
 
             // TODO: use this.Content to load your game content here
@@ -82,18 +107,29 @@ namespace Canada_Hunts
             seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (seconds >= respawntime)
             {
+                for (int i = 1; i <= 5; i++)
                 gooserectangle.Add(new Rectangle(
-                     generator.Next(0, window.Width - 50),
+                     generator.Next(window.Width, window.Width +150),
                      generator.Next(0, 130),
                      50,
                      50
                      ));
+
+                for (int i = 1; i <= 2; i++)
+                    mooserectangle.Add(new Rectangle(
+                         generator.Next(-2000 , -50),
+                         325,
+                        
+                         80,
+                         80
+                         ));
 
                 seconds = 0f;
 
             }
             if (mousestate.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton != ButtonState.Pressed)
             {
+
                 Point mousepoint = new Point(mousestate.X, mousestate.Y);
                 for (int i = gooserectangle.Count - 1; i >= 0; i--)
                 {
@@ -104,6 +140,37 @@ namespace Canada_Hunts
                 }
 
 
+
+
+            }
+            if (mousestate.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton != ButtonState.Pressed)
+            {
+
+                Point mousepoint = new Point(mousestate.X, mousestate.Y);
+                for (int i = mooserectangle.Count -1; i >= 0; i--)
+                {
+                    if (mooserectangle[i].Contains(mousepoint))
+                    {
+                        mooserectangle.RemoveAt(i);
+                    }
+                }
+
+
+
+
+            }
+            Rectangle temp;
+            for (int i = 0; i < gooserectangle.Count; i++)
+            {
+                temp = gooserectangle[i];
+                temp.X += (int)goosespeed.X;
+                gooserectangle[i] = temp;
+            }
+            for (int i = 0; i < mooserectangle.Count; i++)
+            {
+                temp = mooserectangle[i];
+                temp.X += (int)moosespeed.X;
+                mooserectangle[i] = temp;
             }
 
             previousMouseState = mousestate;
@@ -126,9 +193,17 @@ namespace Canada_Hunts
                 _spriteBatch.Draw(gooseTexture, gooserectangle[i], Color.White);
 
             }
+            for (int i = 0; i < mooserectangle.Count; i++)
+            {
+                _spriteBatch.Draw(mooseTexture, mooserectangle[i], Color.White);
+
+            }
 
 
-           
+
+
+
+
 
             // TODO: Add your drawing code here
 
