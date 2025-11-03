9a7e935838bae010b37a2c3dfe2b7228146dd4ea
339713067 <339713067@2260I-L29-023>
2025-11-03 09:39:20 -0500

    not done

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct2D1.Effects;
 using System;
 using System.Collections.Generic;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
@@ -11,15 +12,21 @@ namespace Canada_Hunts
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D scopeTexture;
+        private Vector2 mousePosition;
         Vector2 goosespeed= new Vector2 (-2,0);
 
         Vector2 moosespeed= new Vector2 (2,0);
+
+
+        
 
 
         Random generator;
         List<Rectangle> gooserectangle;
 
         List<Rectangle> mooserectangle;
+        List<Int32> moosehealth;
 
         List<Texture2D> moosetextures;
 
@@ -41,18 +48,30 @@ namespace Canada_Hunts
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
+
+            
+
         }
+
+
+
+      
 
         protected override void Initialize()
         {
             window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
             generator = new Random();
-           
+
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            scopeTexture = Content.Load<Texture2D>("images/scope");
+
             gooserectangle = new List<Rectangle>();
             goosetextures = new List<Texture2D>();
             mooserectangle = new List<Rectangle>();
             moosetextures = new List<Texture2D>();
+            moosehealth = new List<Int32>();
+            
             for (int i = 0; i < 5; i++)
             {
                 gooserectangle.Add(new Rectangle(
@@ -68,12 +87,12 @@ namespace Canada_Hunts
             {
                 mooserectangle.Add(new Rectangle(
                     generator.Next(0, window.Width - 50),
-                    325,
-
-                   
+                    325,   
                     80,
                     80
                     ));
+
+                moosehealth.Add(2);
 
 
             }
@@ -99,6 +118,12 @@ namespace Canada_Hunts
 
         protected override void Update(GameTime gameTime)
         {
+
+            var mouseState = Mouse.GetState();
+            mousePosition = new Vector2(mouseState.X, mouseState.Y);
+
+            this.Window.Title = "MooseCount " + mooserectangle.Count + " MooseHealthcount " + moosehealth.Count;
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             mousestate = Mouse.GetState();
@@ -116,13 +141,19 @@ namespace Canada_Hunts
                      ));
 
                 for (int i = 1; i <= 2; i++)
+                {
                     mooserectangle.Add(new Rectangle(
-                         generator.Next(-2000 , -50),
+                         generator.Next(-2000, -50),
                          325,
-                        
+
                          80,
                          80
                          ));
+
+
+                    moosehealth.Add(2);
+                }
+                    
 
                 seconds = 0f;
 
@@ -131,6 +162,8 @@ namespace Canada_Hunts
             {
 
                 Point mousepoint = new Point(mousestate.X, mousestate.Y);
+
+                // Looks for goose hit
                 for (int i = gooserectangle.Count - 1; i >= 0; i--)
                 {
                     if (gooserectangle[i].Contains(mousepoint))
@@ -140,18 +173,20 @@ namespace Canada_Hunts
                 }
 
 
-
-
-            }
-            if (mousestate.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton != ButtonState.Pressed)
-            {
-
-                Point mousepoint = new Point(mousestate.X, mousestate.Y);
-                for (int i = mooserectangle.Count -1; i >= 0; i--)
+                // Loos for moose hit
+                for (int i = 0; i < mooserectangle.Count ; i++)
                 {
                     if (mooserectangle[i].Contains(mousepoint))
                     {
-                        mooserectangle.RemoveAt(i);
+                        moosehealth[i] -= 1;
+
+                        if (moosehealth[i] == 0)
+                        {
+                            mooserectangle.RemoveAt(i);
+                            moosehealth.RemoveAt(i);
+                            i--;
+                        }
+                        
                     }
                 }
 
@@ -186,8 +221,19 @@ namespace Canada_Hunts
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
 
+           
+
 
             _spriteBatch.Draw(canadabg, window, Color.White);
+
+            _spriteBatch.Draw(
+            scopeTexture,
+            
+            mousePosition - new Vector2(scopeTexture.Width / 2, scopeTexture.Height / 2),
+
+            Color.White
+           
+        );
             for (int i = 0; i < gooserectangle.Count; i++)
             {
                 _spriteBatch.Draw(gooseTexture, gooserectangle[i], Color.White);
